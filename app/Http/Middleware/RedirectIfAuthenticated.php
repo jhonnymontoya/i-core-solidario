372ce5bfc9e9842d1b42759aba171470408d5c03
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2019-10-03 21:19:41 -0500

    Se corrige problema de logueo para usuario de consulta cuando la sesión ya se encuentra abierta

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,9 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Session;
 
 class RedirectIfAuthenticated
 {
+
+    const ADMINISTRADOR = 1;
+    const SOCIO = 2;
+
     /**
      * Handle an incoming request.
      *
@@ -15,10 +20,26 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
-    {
+    public function handle($request, Closure $next, $guard = null) {
         if (Auth::guard($guard)->check()) {
-            return redirect('/dashboard');
+            if(Session::has("tipoUsuario")) {
+                if(Session::get("tipoUsuario") == RedirectIfAuthenticated::ADMINISTRADOR) {
+                    return redirect('/dashboard');
+                }
+                elseif(Session::get("tipoUsuario") == RedirectIfAuthenticated::SOCIO) {
+                    return redirect('/consulta');
+                }
+                else {
+                    Auth::guard($guard)->logout();
+                    Session::invalidate();
+                    return redirect('login');
+                }
+            }
+            else {
+                Auth::guard($guard)->logout();
+                Session::invalidate();
+                return redirect('login');
+            }
         }
 
         return $next($request);
