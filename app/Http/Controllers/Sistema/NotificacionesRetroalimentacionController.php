fd43389a96b4a0f7968755339dd9cda043459047
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-05-29 19:35:38 -0500

    Se avanza con el desarrollo, se agrega método para agregar trama proveniente de SNS Amazon

--- app/Http/Controllers/Sistema/NotificacionesRetroalimentacionController.php
+++ app/Http/Controllers/Sistema/NotificacionesRetroalimentacionController.php
@@ -2,10 +2,44 @@
 
 namespace App\Http\Controllers\Sistema;
 
-use App\Http\Controllers\Controller;
+use Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Sistema\NotificacionRetroalimentacion;
 
 class NotificacionesRetroalimentacionController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:admin')->except(['postSNS']);
+        $this->middleware('verEnt')->except(['postSNS']);
+        $this->middleware('verMenu')->except(['postSNS']);
+    }
+
+    public function index(Request $request)
+    {
+        $notificaciones = NotificacionRetroalimentacion::all();
+        dd("Entro", $notificaciones);
+    }
+
+    public function postSNS(Request $request)
+    {
+        $trama = json_encode($request->all());
+        $obj = NotificacionRetroalimentacion::create(["trama" => $trama]);
+    }
+
+    /**
+     * Establece las rutas
+     */
+    public static function routes()
+    {
+        Route::get(
+            'notificacionesRetroalimentacion',
+            'Sistema/NotificacionesRetroalimentacionController@index'
+        );
+        Route::post(
+            'notificacionesRetroalimentacion/sns',
+            'Sistema/NotificacionesRetroalimentacionController@postSNS'
+        );
+    }
 }
