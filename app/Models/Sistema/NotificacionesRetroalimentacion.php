1b1082f88c9f278ce4049a416e0419fb5b0f7c56
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-05-29 19:06:09 -0500

    Se crea modelo de retroalimentacion de notificaciones de notificaciones

---
+++ app/Models/Sistema/NotificacionesRetroalimentacion.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Models\Sistema;
+
+use App\Traits\FonadminTrait;
+use App\Traits\FonadminModelTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class NotificacionesRetroalimentacion extends Model
+{
+    use SoftDeletes, FonadminTrait, FonadminModelTrait;
+
+    /**
+     * La tabla que está asociada con el modelo
+     * @var String
+     */
+    protected $table = "sistema.notificaciones_retroalimentacion";
+
+    /**
+     * Atributos que se pueden masivamente asignar.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * Atributos que deben ser convertidos a fechas.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Atributos que deben ser convertidos a tipos nativos.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at'        => 'datetime:Y-m-d',
+        'updated_at'        => 'datetime:Y-m-d',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * Getters personalizados
+     */
+
+    /**
+     * Setters Personalizados
+     */
+
+    /**
+     * Scopes
+     */
+
+    /**
+     * Funciones
+     */
+
+    /**
+     * Relaciones Uno a Uno
+     */
+
+    /**
+     * Relaciones Uno a muchos
+     */
+
+    /**
+     * Relaciones Muchos a uno
+     */
+
+    /**
+     * Relaciones Muchos a Muchos
+     */
+
+}
