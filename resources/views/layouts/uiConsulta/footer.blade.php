d63840180a3035b63ced38a494dd50187f0426b8
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-06-20 23:54:48 -0500

    Se cambia la versión para la consulta web

--- resources/views/layouts/uiConsulta/footer.blade.php
+++ resources/views/layouts/uiConsulta/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	<div class="pull-right hidden-xs">
-		<b>Versión</b> 1.6.1
+		<b>Versión</b> 1.0.0
 	</div>
 	<strong>Copyright &copy; 2019-{{ date("Y") }} <a href="https://i-core.co">I-Core <i class="far fa-registered"></i></a>.</strong> Todos los derechos reservados.
 </footer>
