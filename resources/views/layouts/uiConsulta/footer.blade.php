a9055b60c2cbb7457b0aafda4c4469bff4f879d4
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-07-11 19:48:52 -0500

    Se corrige forma de presentar el footer, versión siempre a la derecha

--- resources/views/layouts/uiConsulta/footer.blade.php
+++ resources/views/layouts/uiConsulta/footer.blade.php
@@ -1,6 +1,8 @@
 <footer class="main-footer">
-	<div class="pull-right hidden-xs">
-		<b>Versión</b> 1.0.0
-	</div>
-	<strong>Copyright &copy; 2019-{{ date("Y") }} <a href="https://i-core.co">I-Core <i class="far fa-registered"></i></a>.</strong> Todos los derechos reservados.
+    <div class="row">
+        <div class="col"><strong>Copyright &copy; 2019-{{ date("Y") }} <a href="https://i-core.co">I-Core <i class="far fa-registered"></i></a>.</strong> Todos los derechos reservados.</div>
+    	<div class="pull-right hidden-xs">
+    		<b>Versión</b> 1.0.0
+    	</div>
+    </div>
 </footer>
