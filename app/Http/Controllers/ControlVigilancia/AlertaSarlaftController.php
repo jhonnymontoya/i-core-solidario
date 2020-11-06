88ca68e49f0e4f70c6e761f6e470a588ee31bfe8
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-11-05 21:50:03 -0500

    Se crea controllador de alertas para SARLAFT
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ app/Http/Controllers/ControlVigilancia/AlertaSarlaftController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\ControlVigilancia;
+
+use App\Traits\ICoreTrait;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AlertaSarlaftController extends Controller
+{
+
+}
