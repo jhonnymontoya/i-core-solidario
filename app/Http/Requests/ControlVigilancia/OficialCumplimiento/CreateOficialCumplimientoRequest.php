045e6d71cddeb8c3da306101da6424733dee9f41
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-11-05 20:09:12 -0500

    Se crea clase para request de creación del oficial de cumplimiento
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ app/Http/Requests/ControlVigilancia/OficialCumplimiento/CreateOficialCumplimientoRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\ControlVigilancia\OficialCumplimiento;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateOficialCumplimientoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function($validator) {
+        });
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+        ];
+    }
+}
