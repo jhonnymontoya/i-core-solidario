b42d305472b00302d7ee4c1825e6dd90a90d23a7
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-11-06 00:25:51 -0500

    Se implementa la actualización de alertas SARLAFT
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/Http/Controllers/ControlVigilancia/AlertaSarlaftController.php
+++ app/Http/Controllers/ControlVigilancia/AlertaSarlaftController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\ControlVigilancia;
 
 use Route;
+use Carbon\Carbon;
 use App\Traits\ICoreTrait;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 use App\Models\ControlVigilancia\Alerta;
 use App\Models\ControlVigilancia\OficialCumplimiento;
+use App\Http\Requests\ControlVigilancia\AlertasSalaft\EditAlertaSarlaftRequest;
 
 class AlertaSarlaftController extends Controller
 {
@@ -39,36 +42,88 @@ class AlertaSarlaftController extends Controller
 
     public function edit(Alerta $obj)
     {
-        $msg = sprintf('Ingresó a editar la alerta SARLAFT \'\'', $obj->nombre);
+        $this->objEntidad($obj);
+        $msg = sprintf('Ingresó a editar la alerta SARLAFT \'%s\'', $obj->nombre);
         $this->log($msg);
 
         return view("controlVigilancia.alertasSarlaft.edit")
             ->withAlerta($obj);
     }
 
-    public function update(EditOficialCumplimientoRequest $request)
+    public function update(Alerta $obj, EditAlertaSarlaftRequest $request)
     {
-        $this->log("Creó el oficial de cumplimiento con los siguientes parámetros " . json_encode($request->all()), 'ACTUALIZAR');
-        $alertas = OficialCumplimiento::entidadId()
-            ->activo()
-            ->orderBy("id", "desc")
-            ->first();
+        $msg = "Actualizó la alerta SARLAFT '%s' con los siguientes parámetros %s";
+        $msg = sprintf($msg, $obj->nombre, json_encode($request->all()));
+        $this->log($msg, 'ACTUALIZAR');
 
-        if($alertas == null) {
-            Session::flash("error", "No existe ningún oficial de cumplimiento");
-            return redirect("alertasSarlaft");
+        $modificado = false;
+
+        //DIARIO
+        if($request->diario == 1)
+        {
+            $obj->diario = true;
+            $fecha = Carbon::now()->endOfDay();
+            $obj->fecha_proximo_diario = $fecha;
+            $modificado = true;
+        }
+        else
+        {
+            $obj->diario = false;
+            $obj->fecha_proximo_diario = null;
+            $modificado = true;
         }
 
-        $alertas->delete();
+        //SEMANAL
+        if($request->semanal == 1)
+        {
+            $obj->semanal = true;
+            $fecha = Carbon::now()->endOfWeek();
+            $obj->fecha_proximo_semanal = $fecha;
+            $modificado = true;
+        }
+        else
+        {
+            $obj->semanal = false;
+            $obj->fecha_proximo_semanal = null;
+            $modificado = true;
+        }
 
-        $entidadId = $this->getEntidad()->id;
-        $alertas = new OficialCumplimiento;
-        $alertas->fill($request->all());
-        $alertas->entidad_id = $entidadId;
-        $alertas->email_copia = $request->emailcc;
-        $alertas->save();
+        //MENSUAL
+        if($request->mensual == 1)
+        {
+            $obj->mensual = true;
+            $fecha = Carbon::now()->endOfMonth();
+            $obj->fecha_proximo_mensual = $fecha;
+            $modificado = true;
+        }
+        else
+        {
+            $obj->mensual = false;
+            $obj->fecha_proximo_mensual = null;
+            $modificado = true;
+        }
 
-        Session::flash("message", "Se ha actualizado con éxito el oficial de cumplimiento");
+        //ANUAL
+        if($request->anual == 1)
+        {
+            $obj->anual = true;
+            $fecha = Carbon::now()->endOfYear();
+            $obj->fecha_proximo_anual = $fecha;
+            $modificado = true;
+        }
+        else
+        {
+            $obj->anual = false;
+            $obj->fecha_proximo_anual = null;
+            $modificado = true;
+        }
+
+        if($modificado)
+        {
+            $obj->save();
+        }
+
+        Session::flash("message", "Se ha actualizado con éxito la alerta");
         return redirect("alertasSarlaft");
     }
 
