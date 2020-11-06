287b6ce3c78e7f741b311d17a1efc8538bb2077f
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-11-05 21:39:11 -0500

    Se implementa validador de la edición para el oficial de cumplimiento
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ app/Http/Requests/ControlVigilancia/OficialCumplimiento/EditOficialCumplimientoRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\ControlVigilancia\OficialCumplimiento;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditOficialCumplimientoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nombre'    => 'required|string|min:6|max:255',
+            'email'     => 'required|email:strict,dns|max:255',
+            'emailcc'   => 'nullable|email:strict,dns|max:255'
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function($validator) {
+        });
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'email'     => 'correo electrónico',
+            'emailcc'   => 'copia correo electrónico'
+        ];
+    }
+}
