9d11a896f27ecb49aeca0f2a6253a94d26aa87d8
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-11-05 20:17:11 -0500

    Se crea formulario para listar el oficial de cumplimiento
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ resources/views/controlVigilancia/oficialCumplimplimiento/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.admin')
+
+@section('content')
+{{-- Contenido principal de la página --}}
+<div class="content-wrapper">
+	<section class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-6">
+					<h1>
+						Oficial de cumplimiento
+						<small>SARLAFT</small>
+					</h1>
+				</div>
+				<div class="col-6">
+					<ol class="breadcrumb float-sm-right">
+						<li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+						<li class="breadcrumb-item"><a href="#"> SARLAFT</a></li>
+						<li class="breadcrumb-item active">Oficial de cumplimiento</li>
+					</ol>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="content">
+		@if (Session::has('message'))
+			<div class="alert alert-success alert-dismissible" data-closable>
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true"><i class="fa fa-times"></i></button>
+			   {{ Session::get('message') }}
+			</div>
+		@endif
+		<div class="row">
+			<div class="col-md-2">
+				<a href="{{ url('oficialCumplimiento/create') }}" class="btn btn-outline-primary">Crear nuevo</a>
+			</div>
+		</div>
+		<br>
+		<div class="container-fluid">
+			<div class="card card-{{ $oficialCumplimiento != null ?'primary':'danger' }} card-outline">
+				<div class="card-header with-border">
+					<h3 class="card-title">Oficial de cumplimiento</h3>
+				</div>
+				<div class="card-body">
+					@if($oficialCumplimiento == null)
+						<p>
+							<div class="row">
+								<div class="col-md-12">
+									No se encontró nungún oficial de cumplimiento <a href="{{ url('oficialCumplimiento/create') }}" class="btn btn-outline-primary btn-sm">crear uno nuevo</a>
+								</div>
+							</div>
+						</p>
+					@else
+						Existe
+					@endif
+				</div>
+				<div class="card-footer">
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+{{-- Fin de contenido principal de la página --}}
+@endsection
+
+@push('style')
+@endpush
+
+@push('scripts')
+<script type="text/javascript">
+	$(function(){
+		$("input[name='name']").enfocar();
+		$(window).formularioCrear("{{ url('tipoIdentificacion/create') }}");
+	});
+</script>
+@endpush
