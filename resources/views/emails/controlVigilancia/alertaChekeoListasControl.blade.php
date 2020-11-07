48f81eab48dfc412acee16659fb55af7b0142367
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2020-11-06 19:22:43 -0500

    Se agrega linea en el cuerdpo del correo que explica de que es el correo
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- resources/views/emails/controlVigilancia/alertaChekeoListasControl.blade.php
+++ resources/views/emails/controlVigilancia/alertaChekeoListasControl.blade.php
@@ -3,6 +3,8 @@
 
 Este mensaje ha sido enviado automáticamente como herramienta para desarrollar tu gestión como oficial de cumplimiento en la entidad {{ $sigla }}.
 
+Adjunto encontrará el reporte {{ $periodicidad }} del chekeo de listas de control.
+
 Cordialmente,<br>
 {{ $sigla }}
 @endcomponent
