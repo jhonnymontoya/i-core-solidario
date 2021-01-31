ac83e80361d03f570806382186f1130dd7f023e9
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-01-31 14:07:26 -0500

    Se agregan funciones para averiguar si el email es valido y otra función para enmascarar el correo
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/Models/General/Contacto.php
+++ app/Models/General/Contacto.php
@@ -2,142 +2,161 @@
 
 namespace App\Models\General;
 
-use App\Models\Socios\TipoVivienda;
-use App\Traits\ICoreModelTrait;
+use Validator;
 use App\Traits\ICoreTrait;
+use App\Traits\ICoreModelTrait;
+use App\Models\Socios\TipoVivienda;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contacto extends Model
 {
-	use SoftDeletes, ICoreTrait, ICoreModelTrait;
+    use SoftDeletes, ICoreTrait, ICoreModelTrait;
 
-	/**
-	 * La tabla que está asociada con el modelo
-	 * @var String
-	 */
-	protected $table = "general.contactos";
+    /**
+     * La tabla que está asociada con el modelo
+     * @var String
+     */
+    protected $table = "general.contactos";
 
-	/**
-	 * Atributos que se pueden masivamente asignar.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'tipo_contacto',
-		'direccion',
-		'estrato',
-		'tipo_vivienda',
-		'email',
-		'telefono',
-		'extension',
-		'movil',
-		'es_preferido',
-	];
+    /**
+     * Atributos que se pueden masivamente asignar.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'tipo_contacto',
+        'direccion',
+        'estrato',
+        'tipo_vivienda',
+        'email',
+        'telefono',
+        'extension',
+        'movil',
+        'es_preferido',
+    ];
 
-	/**
-	 * Atributos que deben ser convertidos a fechas.
-	 *
-	 * @var array
-	 */
-	protected $dates = [
-		'created_at',
-		'updated_at',
-		'deleted_at'
-	];
+    /**
+     * Atributos que deben ser convertidos a fechas.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 
-	/**
-	 * Atributos que deben ser convertidos a tipos nativos.
-	 *
-	 * @var array
-	 */
-	protected $casts = [
-		'es_preferido' => 'boolean',
-	];
+    /**
+     * Atributos que deben ser convertidos a tipos nativos.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'es_preferido' => 'boolean',
+    ];
 
-	/**
-	 * Getters personalizados
-	 */
+    /**
+     * Getters personalizados
+     */
 
-	/**
-	 * Setters Personalizados
-	 */
+    /**
+     * Setters Personalizados
+     */
 
-	/**
-	 * Se guarda el email en minúscula
-	 * @param type $value
-	 * @return type
-	 */
-	public function setEmailAttribute($value) {
-		$this->attributes['email'] = mb_convert_case($value, MB_CASE_LOWER, "UTF-8");
-	}
+    /**
+     * Se guarda el email en minúscula
+     * @param type $value
+     * @return type
+     */
+    public function setEmailAttribute($value) {
+        $this->attributes['email'] = mb_convert_case($value, MB_CASE_LOWER, "UTF-8");
+    }
 
-	/**
-	 * Scopes
-	 */
+    /**
+     * Scopes
+     */
 
-	/**
-	 * Funciones
-	 */
+    /**
+     * Funciones
+     */
 
-	public function hayCampos() {
-		$conCampos = false;
-		$conCampos = empty($this->attributes['ciudad_id'])?false:true;
-		if($conCampos)return true;
-		$conCampos = empty($this->attributes['direccion'])?false:true;
-		if($conCampos)return true;
-		$conCampos = empty($this->attributes['estrato'])?false:true;
-		if($conCampos)return true;
-		$conCampos = empty($this->attributes['tipo_vivienda_id'])?false:true;
-		if($conCampos)return true;
-		$conCampos = empty($this->attributes['email'])?false:true;
-		if($conCampos)return true;
-		$conCampos = empty($this->attributes['telefono'])?false:true;
-		if($conCampos)return true;
-		$conCampos = empty($this->attributes['extension'])?false:true;
-		if($conCampos)return true;
-		$conCampos = empty($this->attributes['movil'])?false:true;
-		if($conCampos)return true;
+    public function hayCampos() {
+        $conCampos = false;
+        $conCampos = empty($this->attributes['ciudad_id'])?false:true;
+        if($conCampos)return true;
+        $conCampos = empty($this->attributes['direccion'])?false:true;
+        if($conCampos)return true;
+        $conCampos = empty($this->attributes['estrato'])?false:true;
+        if($conCampos)return true;
+        $conCampos = empty($this->attributes['tipo_vivienda_id'])?false:true;
+        if($conCampos)return true;
+        $conCampos = empty($this->attributes['email'])?false:true;
+        if($conCampos)return true;
+        $conCampos = empty($this->attributes['telefono'])?false:true;
+        if($conCampos)return true;
+        $conCampos = empty($this->attributes['extension'])?false:true;
+        if($conCampos)return true;
+        $conCampos = empty($this->attributes['movil'])?false:true;
+        if($conCampos)return true;
 
-		return false;
-	}
+        return false;
+    }
 
-	public function getTelefono()
-	{
-		if(!empty($this->attributes["movil"])){
-			return $this->attributes["movil"];
-		}
-		elseif(!empty($this->attributes["telefono"])){
-			return $this->attributes["telefono"];
-		}
-		return "";
-	}
+    public function getTelefono()
+    {
+        if(!empty($this->attributes["movil"])){
+            return $this->attributes["movil"];
+        }
+        elseif(!empty($this->attributes["telefono"])){
+            return $this->attributes["telefono"];
+        }
+        return "";
+    }
 
-	/**
-	 * Relaciones Uno a Uno
-	 */
+    public function emailEnmascarado()
+    {
+        list($nombre, $dominio) = explode("@", $this->attributes["email"]);
+        $nombre = substr($nombre, 0, 2) . str_pad("", strlen($nombre) - 2, "*");
+        $dominio = substr($dominio, 0, 2) . str_pad("", strlen($dominio) - 2, "*");
+        $correoEnmascarado = Str::limit($nombre . "@" . $dominio, 40);
+        return $correoEnmascarado;
+    }
 
-	/**
-	 * Relaciones Uno a muchos
-	 */
+    public function esEmailValido()
+    {
+        $val = Validator::make(
+            ["email" => $contacto->email],
+            ["email" => "bail|required|email"]
+        );
+        return $val->fails();
+    }
 
-	/**
-	 * Relaciones Muchos a uno
-	 */
+    /**
+     * Relaciones Uno a Uno
+     */
 
-	public function ciudad() {
-		return $this->belongsTo(Ciudad::class, 'ciudad_id', 'id');
-	}
+    /**
+     * Relaciones Uno a muchos
+     */
 
-	public function tercero() {
-		return $this->belongsTo(Tercero::class, 'tercero_id', 'id');
-	}
+    /**
+     * Relaciones Muchos a uno
+     */
 
-	public function tipoVivienda() {
-		return $this->belongsTo(TipoVivienda::class, 'tipo_vivienda_id', 'id');
-	}
+    public function ciudad() {
+        return $this->belongsTo(Ciudad::class, 'ciudad_id', 'id');
+    }
 
-	/**
-	 * Relaciones Muchos a Muchos
-	 */
+    public function tercero() {
+        return $this->belongsTo(Tercero::class, 'tercero_id', 'id');
+    }
+
+    public function tipoVivienda() {
+        return $this->belongsTo(TipoVivienda::class, 'tipo_vivienda_id', 'id');
+    }
+
+    /**
+     * Relaciones Muchos a Muchos
+     */
 }
