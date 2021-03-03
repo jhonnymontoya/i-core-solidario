2328f5844d5fe5dc80588c324e821154027d26bd
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-03-03 11:31:39 -0500

    Se implementa el guardar de configuración de extracto social
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/Http/Controllers/Reportes/ConfiguracionExtractoSocialController.php
+++ app/Http/Controllers/Reportes/ConfiguracionExtractoSocialController.php
@@ -1,12 +1,13 @@
 <?php
-
 namespace App\Http\Controllers\Reportes;
 
 use Route;
 use App\Traits\ICoreTrait;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 use App\Models\Reportes\ConfiguracionExtractoSocial;
+use App\Http\Requests\Reportes\ConfiguracionExtractoSocial\CreateConfiguracionExtractoSocialRequest;
 
 class ConfiguracionExtractoSocialController extends Controller
 {
@@ -19,12 +20,13 @@ class ConfiguracionExtractoSocialController extends Controller
         $this->middleware('verMenu');
     }
 
-    public function index(Request $request)
+    public function index()
     {
-        $this->log("Ingresó a la configuración de extracto social con los siguientes parámetros " . json_encode($request->all()));
+        $this->log("Ingresó a la configuración de extracto social");
         $configuraciones = ConfiguracionExtractoSocial::entidadId()
             ->orderBy("anio", "desc")
             ->paginate();
+
         return view("reportes.extractoSocial.index")
             ->withConfiguraciones($configuraciones);
     }
@@ -35,10 +37,23 @@ class ConfiguracionExtractoSocialController extends Controller
         return view("reportes.extractoSocial.create");
     }
 
-    public function store(Request $request)
+    public function store(CreateConfiguracionExtractoSocialRequest $request)
     {
-        $this->log("Creó configuración de extracto social con los siguientes parámetros " . json_encode($request->all()), "CREAR");
-        dd("store", $request->all());
+        $msg = "Creó configuración de extracto social con los siguientes parámetros %s";
+        $msg = sprintf($msg, json_encode($request->all()));
+        $this->log($msg, "CREAR");
+
+        $entidad = new ConfiguracionExtractoSocial();
+
+        $entidad->fill($request->all());
+        $entidad->entidad_id = $this->getEntidad()->id;
+
+        $entidad->save();
+
+        $msg = "Se ha guardado la configuración de extracto social para el año '%s'";
+        $msg = sprintf($msg, $entidad->anio);
+        Session::flash('message', $msg);
+        return redirect('extractoSocial');
     }
 
     public function edit(ConfiguracionExtractoSocial $obj)
@@ -47,6 +62,7 @@ class ConfiguracionExtractoSocialController extends Controller
         $this->log(sprintf($msg, $obj->id));
         $this->objEntidad($obj);
         dd("edit");
+        return view("reportes.extractoSocial.create");
     }
 
     public function update(ConfiguracionExtractoSocial $obj, Request $request)
@@ -55,15 +71,42 @@ class ConfiguracionExtractoSocialController extends Controller
         $msg = sprintf($msg, $obj->id, json_encode($request->all()));
         $this->log($msg, "ACTUALIZAR");
         $this->objEntidad($obj);
-        dd("update");
+        dd("update", $request->all());
+
+        $obj->fill($request->all());
+        $obj->save();
+
+        $msg = "Se ha actualizado extractoSocial '%s'";
+        $msg = sprintf($msg, $obj->nombre);
+        Session::flash('message', $msg);
+        return redirect('extractoSocial');
     }
 
     public static function routes()
     {
-        Route::get('extractoSocial', 'Reportes\ConfiguracionExtractoSocialController@index');
-        Route::get('extractoSocial/create', 'Reportes\ConfiguracionExtractoSocialController@create');
-        Route::post('extractoSocial', 'Reportes\ConfiguracionExtractoSocialController@store');
-        Route::get('extractoSocial/{obj}/edit', 'Reportes\ConfiguracionExtractoSocialController@edit')->name("extractoSocial.edit");
-        Route::put('extractoSocial/{obj}', 'Reportes\ConfiguracionExtractoSocialController@update');
+        Route::get(
+            'extractoSocial',
+            'Reportes\ConfiguracionExtractoSocialController@index'
+        );
+
+        Route::get(
+            'extractoSocial/create',
+            'Reportes\ConfiguracionExtractoSocialController@create'
+        );
+
+        Route::post(
+            'extractoSocial',
+            'Reportes\ConfiguracionExtractoSocialController@store'
+        );
+
+        Route::get(
+            'extractoSocial/{obj}/edit',
+            'Reportes\ConfiguracionExtractoSocialController@edit'
+        )->name("extractoSocial.edit");
+
+        Route::put(
+            'extractoSocial/{obj}',
+            'Reportes\ConfiguracionExtractoSocialController@update'
+        );
     }
 }
