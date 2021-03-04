afa1e28b417de48f8725749e88aecdecce5a6709
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-03-04 18:00:51 -0500

    Se crea Clase para obtener certificado de extracto social
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

---
+++ app/Certificados/CertificadoExtractoSocial.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Certificados;
+
+use PDF;
+use Auth;
+use Exception;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+
+class CertificadoExtractoSocial
+{
+
+    private $socio = null;
+    private $anio = null;
+    private $vista = "pdf.socios.certificadoTributario";
+    private $path = null;
+
+    /**
+     * Constructor
+     */
+    public function __construct($socio, $anio) {
+        $this->socio = $socio;
+        $this->anio = $anio;
+        $entidad = $this->getEntidad();
+        $path = $this->getPath();
+
+        $data = DB::select("EXEC socios.sp_certificado_tributario ?, ?", [$socio->id, $anio]);
+        $pdf = PDF::loadView($this->vista, compact("socio", "anio", "entidad", "data"))
+            ->setPaper('letter', 'portait')
+            ->setWarnings(false);
+        $pdf->save($path);
+        $this->path = $path;
+    }
+
+    /**
+     * Retorna la entidad actual
+     * @return type
+     */
+    protected function getEntidad() {
+        $entidad = $this->socio->tercero->entidad;
+        return $entidad;
+    }
+
+    public function getRuta() {
+        return $this->path;
+    }
+
+    public function getPath() {
+        $tercero = $this->socio->tercero;
+        $nombre = sprintf("%s %s", Str::random(10), $tercero->nombre_completo);
+        $nombre = Str::slug($nombre, "_") . ".pdf";
+        $path = "app%spdf%s%s";
+        $path = sprintf($path, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $nombre);
+        $path = storage_path($path);
+        return $path;
+    }
+}
