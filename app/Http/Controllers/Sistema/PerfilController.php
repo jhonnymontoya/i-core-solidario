92853622bd91222e5ff0e0d71a86b2b0ddd850df
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-08-17 14:47:12 -0500

    Se agrega funcionalidad para filtrar en index perfiles de root
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/Http/Controllers/Sistema/PerfilController.php
+++ app/Http/Controllers/Sistema/PerfilController.php
@@ -2,21 +2,19 @@
 
 namespace App\Http\Controllers\Sistema;
 
+use DB;
+use Log;
+use Auth;
+use Route;
+use App\Models\Sistema\Menu;
 use Illuminate\Http\Request;
+use App\Models\Sistema\Perfil;
+use App\Models\General\Entidad;
+use App\Models\Sistema\MenuPerfil;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
-use Route;
-
-use App\Models\Sistema\Menu;
-use App\Models\Sistema\Perfil;
-use App\Models\Sistema\MenuPerfil;
-use App\Models\General\Entidad;
-
-use App\Http\Requests\Sistema\Perfil\CreatePerfilRequest;
 use App\Http\Requests\Sistema\Perfil\EditPerfilRequest;
-
-use Log;
-use DB;
+use App\Http\Requests\Sistema\Perfil\CreatePerfilRequest;
 
 class PerfilController extends Controller
 {
@@ -29,23 +27,48 @@ class PerfilController extends Controller
 	public function index(Request $request) {
 		$entidades = Entidad::with('terceroEntidad')->get();
 		$listaEntidades = array();
-		foreach($entidades as $entidad)$listaEntidades[$entidad->id] = $entidad->terceroEntidad->nombre_corto;
-		$perfiles = Perfil::with(['entidad', 'menus'])->entidadId($request->entidad)->search($request->name)->activo($request->estado)->paginate();
-		return view('sistema.perfil.index')->withPerfiles($perfiles)->withEntidades($listaEntidades);
+		foreach($entidades as $entidad)
+			$listaEntidades[$entidad->id] = $entidad->terceroEntidad->nombre_corto;
+
+		$perfiles = Perfil::with(['entidad', 'menus'])
+			->entidadId($request->entidad)
+			->search($request->name)
+			->activo($request->estado);
+
+		if($this->esSesionRoot() == false){
+            $usuarios->whereEsRoot(false);
+        }
+
+		$perfiles = $perfiles->paginate();
+
+		return view('sistema.perfil.index')
+			->withPerfiles($perfiles)
+			->withEntidades($listaEntidades);
 	}
 
 	public function create() {
-		$entidades = Entidad::activa()->get()->sortBy('terceroEntidad.razon_social')->pluck('terceroEntidad.razon_social', 'id');
+		$entidades = Entidad::activa()
+			->get()
+			->sortBy('terceroEntidad.razon_social')
+			->pluck('terceroEntidad.razon_social', 'id');
+
 		$menus = Menu::listarTodos();
-		return view('sistema.perfil.create')->withMenuList($menus)->withEntidades($entidades);
+
+		return view('sistema.perfil.create')
+			->withMenuList($menus)
+			->withEntidades($entidades);
 	}
 
 	public function store(CreatePerfilRequest $request) {
 		$perfil = new Perfil;
 		try {
 			DB::transaction(function() use($perfil, $request){
-				$perfil = Entidad::find($request->entidad_id)->perfiles()->create($request->all());
-				$perfil->menus()->attach(Menu::find($request->menus));
+				$perfil = Entidad::find($request->entidad_id)
+					->perfiles()
+					->create($request->all());
+
+				$perfil->menus()
+					->attach(Menu::find($request->menus));
 			});
 		}
 		catch(Exception $e) {
@@ -58,15 +81,15 @@ class PerfilController extends Controller
 
 	public function edit(Perfil $obj) {
 		$entidades = Entidad::activa()
-						->get()
-						->sortBy('terceroEntidad.razon_social')
-						->pluck('terceroEntidad.razon_social', 'id');
+			->get()
+			->sortBy('terceroEntidad.razon_social')
+			->pluck('terceroEntidad.razon_social', 'id');
 
 		$menus = Menu::listarTodos();
 		return view('sistema.perfil.edit')
-					->withMenuList($menus)
-					->withPerfil($obj)
-					->withEntidades($entidades);
+			->withMenuList($menus)
+			->withPerfil($obj)
+			->withEntidades($entidades);
 	}
 
 	public function update(EditPerfilRequest $request, Perfil $obj) {
@@ -86,11 +109,17 @@ class PerfilController extends Controller
 		return redirect('perfil');
 	}
 
+	private function esSesionRoot()
+    {
+        return Auth::user()->es_root;
+    }
+
 	public static function routes() {
 		Route::get('perfil', 'Sistema\PerfilController@index');
 		Route::get('perfil/create', 'Sistema\PerfilController@create');
 		Route::post('perfil', 'Sistema\PerfilController@store');
-		Route::get('perfil/{obj}/edit', 'Sistema\PerfilController@edit')->name('perfilEdit');
+		Route::get('perfil/{obj}/edit', 'Sistema\PerfilController@edit')
+			->name('perfilEdit');
 		Route::put('perfil/{obj}', 'Sistema\PerfilController@update');
 	}
 }
