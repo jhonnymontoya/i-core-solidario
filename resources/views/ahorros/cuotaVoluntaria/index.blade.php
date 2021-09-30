d68ba3c5d86aa10dac289b6f1e4e62013d04b106
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-09-30 18:14:31 -0500

    Se convierte de espacios a tabs
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- resources/views/ahorros/cuotaVoluntaria/index.blade.php
+++ resources/views/ahorros/cuotaVoluntaria/index.blade.php
@@ -3,161 +3,161 @@
 @section('content')
 {{-- Contenido principal de la página --}}
 <div class="content-wrapper">
-	<section class="content-header">
-		<div class="container-fluid">
-			<div class="row mb-2">
-				<div class="col-6">
-					<h1>
-						Cuotas voluntarias
-						<small>Ahorros</small>
-					</h1>
-				</div>
-				<div class="col-6">
-					<ol class="breadcrumb float-sm-right">
-						<li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
-						<li class="breadcrumb-item"><a href="#"> Ahorros</a></li>
-						<li class="breadcrumb-item active">Cuotas voluntarias</li>
-					</ol>
-				</div>
-			</div>
-		</div>
-	</section>
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-6">
+                    <h1>
+                        Cuotas voluntarias
+                        <small>Ahorros</small>
+                    </h1>
+                </div>
+                <div class="col-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+                        <li class="breadcrumb-item"><a href="#"> Ahorros</a></li>
+                        <li class="breadcrumb-item active">Cuotas voluntarias</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
 
-	<section class="content">
-		@if (Session::has('message'))
-			<div class="alert alert-success alert-dismissible" data-closable>
-				<button type="button" class="close" data-dismiss="alert" aria-hidden="true"><i class="fa fa-times"></i></button>
-			   {{ Session::get('message') }}
-			</div>
-		@endif
-		<br>
-		<div class="container-fluid">
-			<div class="card card-primary card-outline">
-				<div class="card-header with-border">
-					<h3 class="card-title">Cuotas voluntarias</h3>
-				</div>
-				<div class="card-body">
-					{!! Form::model(Request::only('socio'), ['url' => 'cuotaVoluntaria', 'method' => 'GET', 'role' => 'search']) !!}
-					<div class="row">
-						<div class="col-md-11">
-							<div class="form-group">
-								<label class="control-label">
-									@if ($errors->has('tipo_comprobante_id'))
-										<i class="fa fa-times-circle-o"></i>
-									@endif
-									Seleccione socio
-								</label>
-								{!! Form::select('socio', [], null, ['class' => 'form-control select2', 'placeholder' => 'Seleccione socio']) !!}
-							</div>
-						</div>
-						<div class="col-md-1 col-sm-12">
-							<label class="control-label">&nbsp;</label>
-							<br>
-							<button type="submit" class="btn btn-outline-success"><i class="fa fa-search"></i></button>
-						</div>
-					</div>
-					{!! Form::close() !!}
-					@if($socio)
-						<br>
-						<div class="row">
-							<div class="col-md-12">
-								<label>Cuotas voluntarias para:</label> {{$socio->tercero->nombre_completo}}
-								<a href="{{ route('cuotaVoluntariaCreate', $socio) }}" class="btn btn-outline-success float-right">Agregar</a>
-							</div>
-						</div>
-						<br><br>
-						@if($socio->cuotasVoluntarias->count())
-							<div class="row">
-								<div class="col-md-12 table-responsive">
-									<table class="table table-striped table-hover">
-										<thead>
-											<tr>
-												<th>Modalidad</th>
-												<th>Variable</th>
-												<th class="text-right">Valor</th>
-												<th class="text-right">Monto cuota</th>
-												<th>Periodicidad</th>
-												<th>Periodo inicial</th>
-												<th>Periodo final</th>
-												<th></th>
-											</tr>
-										</thead>
-										<tbody>
-											@foreach($socio->cuotasVoluntarias as $cuota)
-												<?php
-													$tipo_calculo = "";
-													$montoCuota = 0;
-													$valor = 0;
-													switch ($cuota->factor_calculo) {
-														case 'PORCENTAJESUELDO':
-															$tipo_calculo = "% Sueldo";
-															$valor = number_format($cuota->valor) . '%';
-															$montoCuota = ($socio->sueldo_mes * $cuota->valor) / 100;
-															$montoCuota = number_format($montoCuota);
-															break;
-														case 'PORCENTAJESMMLV':
-															$tipo_calculo = "% SMMLV";
-															$valor = number_format($cuota->valor) . '%';
-															break;
-														case 'VALORFIJO':
-															$tipo_calculo = "Valor fijo";
-															$valor = '$' . number_format($cuota->valor);
-															$montoCuota = $cuota->valor;
-															$montoCuota = number_format($montoCuota);
-															break;												
-														default:
-															break;
-													}
-													$periodicidad = "";
-													switch($cuota->periodicidad)
-													{
-														case 'DIARIO': $periodicidad = 'Diario'; break;
-														case 'SEMANAL': $periodicidad = 'Semanal'; break;
-														case 'DECADAL': $periodicidad = 'Decadal'; break;
-														case 'CATORCENAL': $periodicidad = 'Catorcenal'; break;
-														case 'QUINCENAL': $periodicidad = 'Quincenal'; break;
-														case 'MENSUAL': $periodicidad = 'Mensual'; break;
-														case 'BIMESTRAL': $periodicidad = 'Bimestral'; break;
-														case 'TRIMESTRAL': $periodicidad = 'Trimestral'; break;
-														case 'CUATRIMESTRAL': $periodicidad = 'Cuatrimestral'; break;
-														case 'SEMESTRAL': $periodicidad = 'Semestral'; break;
-														case 'ANUAL': $periodicidad = 'Anual'; break;
-													}
-												?>
-												<tr>
-													<td>{{ $cuota->modalidadAhorro->codigo . ' - ' . $cuota->modalidadAhorro->nombre }}</td>
-													<td>{{ $tipo_calculo }}</td>
-													<td class="text-right">{{ $valor }}</td>
-													<td class="text-right">${{ $montoCuota }}</td>
-													<td>{{ $periodicidad }}</td>
-													<td>{{ empty($cuota->periodo_inicial) ? '' : $cuota->periodo_inicial->toFormattedDateString() }}</td>
-													<td>{{ empty($cuota->periodo_final) ? '' : $cuota->periodo_final->toFormattedDateString() }}</td>
-													<td>
-														<a href="{{ route('cuotaVoluntariaDelete', $cuota->id) }}" class="btn btn-outline-danger btn-sm"><i class="far fa-trash-alt"></i></a>
-													</td>
-												</tr>
-											@endforeach
-										</tbody>
-									</table>
-								</div>
-							</div>
-						@else
-							<div class="row">
-								<div class="col-md-12">
-									<p>Sin cuotas voluntarias</p>
-								</div>
-							</div>
-						@endif
-					@endif
-				</div>
-				<div class="card-footer">
-					@if($socio)
-						<span class="badge badge-pill badge-primary">{{ $socio->cuotasVoluntarias->count() }}</span>&nbsp;elementos.
-					@endif
-				</div>
-			</div>
-		</div>
-	</section>
+    <section class="content">
+        @if (Session::has('message'))
+            <div class="alert alert-success alert-dismissible" data-closable>
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true"><i class="fa fa-times"></i></button>
+               {{ Session::get('message') }}
+            </div>
+        @endif
+        <br>
+        <div class="container-fluid">
+            <div class="card card-primary card-outline">
+                <div class="card-header with-border">
+                    <h3 class="card-title">Cuotas voluntarias</h3>
+                </div>
+                <div class="card-body">
+                    {!! Form::model(Request::only('socio'), ['url' => 'cuotaVoluntaria', 'method' => 'GET', 'role' => 'search']) !!}
+                    <div class="row">
+                        <div class="col-md-11">
+                            <div class="form-group">
+                                <label class="control-label">
+                                    @if ($errors->has('tipo_comprobante_id'))
+                                        <i class="fa fa-times-circle-o"></i>
+                                    @endif
+                                    Seleccione socio
+                                </label>
+                                {!! Form::select('socio', [], null, ['class' => 'form-control select2', 'placeholder' => 'Seleccione socio']) !!}
+                            </div>
+                        </div>
+                        <div class="col-md-1 col-sm-12">
+                            <label class="control-label">&nbsp;</label>
+                            <br>
+                            <button type="submit" class="btn btn-outline-success"><i class="fa fa-search"></i></button>
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
+                    @if($socio)
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label>Cuotas voluntarias para:</label> {{$socio->tercero->nombre_completo}}
+                                <a href="{{ route('cuotaVoluntariaCreate', $socio) }}" class="btn btn-outline-success float-right">Agregar</a>
+                            </div>
+                        </div>
+                        <br><br>
+                        @if($socio->cuotasVoluntarias->count())
+                            <div class="row">
+                                <div class="col-md-12 table-responsive">
+                                    <table class="table table-striped table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>Modalidad</th>
+                                                <th>Variable</th>
+                                                <th class="text-right">Valor</th>
+                                                <th class="text-right">Monto cuota</th>
+                                                <th>Periodicidad</th>
+                                                <th>Periodo inicial</th>
+                                                <th>Periodo final</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($socio->cuotasVoluntarias as $cuota)
+                                                <?php
+                                                    $tipo_calculo = "";
+                                                    $montoCuota = 0;
+                                                    $valor = 0;
+                                                    switch ($cuota->factor_calculo) {
+                                                        case 'PORCENTAJESUELDO':
+                                                            $tipo_calculo = "% Sueldo";
+                                                            $valor = number_format($cuota->valor) . '%';
+                                                            $montoCuota = ($socio->sueldo_mes * $cuota->valor) / 100;
+                                                            $montoCuota = number_format($montoCuota);
+                                                            break;
+                                                        case 'PORCENTAJESMMLV':
+                                                            $tipo_calculo = "% SMMLV";
+                                                            $valor = number_format($cuota->valor) . '%';
+                                                            break;
+                                                        case 'VALORFIJO':
+                                                            $tipo_calculo = "Valor fijo";
+                                                            $valor = '$' . number_format($cuota->valor);
+                                                            $montoCuota = $cuota->valor;
+                                                            $montoCuota = number_format($montoCuota);
+                                                            break;
+                                                        default:
+                                                            break;
+                                                    }
+                                                    $periodicidad = "";
+                                                    switch($cuota->periodicidad)
+                                                    {
+                                                        case 'DIARIO': $periodicidad = 'Diario'; break;
+                                                        case 'SEMANAL': $periodicidad = 'Semanal'; break;
+                                                        case 'DECADAL': $periodicidad = 'Decadal'; break;
+                                                        case 'CATORCENAL': $periodicidad = 'Catorcenal'; break;
+                                                        case 'QUINCENAL': $periodicidad = 'Quincenal'; break;
+                                                        case 'MENSUAL': $periodicidad = 'Mensual'; break;
+                                                        case 'BIMESTRAL': $periodicidad = 'Bimestral'; break;
+                                                        case 'TRIMESTRAL': $periodicidad = 'Trimestral'; break;
+                                                        case 'CUATRIMESTRAL': $periodicidad = 'Cuatrimestral'; break;
+                                                        case 'SEMESTRAL': $periodicidad = 'Semestral'; break;
+                                                        case 'ANUAL': $periodicidad = 'Anual'; break;
+                                                    }
+                                                ?>
+                                                <tr>
+                                                    <td>{{ $cuota->modalidadAhorro->codigo . ' - ' . $cuota->modalidadAhorro->nombre }}</td>
+                                                    <td>{{ $tipo_calculo }}</td>
+                                                    <td class="text-right">{{ $valor }}</td>
+                                                    <td class="text-right">${{ $montoCuota }}</td>
+                                                    <td>{{ $periodicidad }}</td>
+                                                    <td>{{ empty($cuota->periodo_inicial) ? '' : $cuota->periodo_inicial->toFormattedDateString() }}</td>
+                                                    <td>{{ empty($cuota->periodo_final) ? '' : $cuota->periodo_final->toFormattedDateString() }}</td>
+                                                    <td>
+                                                        <a href="{{ route('cuotaVoluntariaDelete', $cuota->id) }}" class="btn btn-outline-danger btn-sm"><i class="far fa-trash-alt"></i></a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        @else
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>Sin cuotas voluntarias</p>
+                                </div>
+                            </div>
+                        @endif
+                    @endif
+                </div>
+                <div class="card-footer">
+                    @if($socio)
+                        <span class="badge badge-pill badge-primary">{{ $socio->cuotasVoluntarias->count() }}</span>&nbsp;elementos.
+                    @endif
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 {{-- Fin de contenido principal de la página --}}
 @endsection
@@ -167,8 +167,8 @@
 
 @push('scripts')
 <script type="text/javascript">
-	$(function(){
-		$("select[name='socio']").selectAjax("{{ url('socio/getSocio') }}", {id:"{{Request::get('socio')}}"});
-	});
+    $(function(){
+        $("select[name='socio']").selectAjax("{{ url('socio/getSocio') }}", {id:"{{Request::get('socio')}}"});
+    });
 </script>
 @endpush
