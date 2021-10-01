c93e8f60aa39eca2b45fa3de0f523adfeab6a125
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-09-30 19:21:06 -0500

    Se cambia de tabs por espacios
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- app/Models/Ahorros/CuotaVoluntaria.php
+++ app/Models/Ahorros/CuotaVoluntaria.php
@@ -13,103 +13,103 @@ class CuotaVoluntaria extends Model
 {
     use SoftDeletes, ICoreTrait, ICoreModelTrait;
 
-	/**
-	 * La tabla que está asociada con el modelo
-	 * @var String
-	 */
-	protected $table = "ahorros.cuotas_voluntarias";
+    /**
+     * La tabla que está asociada con el modelo
+     * @var String
+     */
+    protected $table = "ahorros.cuotas_voluntarias";
 
-	/**
-	 * Atributos que se pueden masivamente asignar.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'socio_id',
-		'modalidad_ahorro_id',
-		'factor_calculo',
-		'valor',
-		'periodicidad',
-		'periodo_inicial',
-		'periodo_final',
-	];
+    /**
+     * Atributos que se pueden masivamente asignar.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'socio_id',
+        'modalidad_ahorro_id',
+        'factor_calculo',
+        'valor',
+        'periodicidad',
+        'periodo_inicial',
+        'periodo_final',
+    ];
 
-	/**
-	 * Atributos que deben ser convertidos a fechas.
-	 *
-	 * @var array
-	 */
-	protected $dates = [
-		'periodo_inicial',
-		'periodo_final',
-		'created_at',
-		'updated_at',
-		'deleted_at'
-	];
+    /**
+     * Atributos que deben ser convertidos a fechas.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'periodo_inicial',
+        'periodo_final',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 
-	/**
-	 * Atributos que deben ser convertidos a tipos nativos.
-	 *
-	 * @var array
-	 */
-	protected $casts = [
-	];
+    /**
+     * Atributos que deben ser convertidos a tipos nativos.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
 
-	/**
-	 * Getters personalizados
-	 */
-	
-	/**
-	 * Setters Personalizados
-	 */
+    /**
+     * Getters personalizados
+     */
 
-	public function setPeriodoInicialAttribute($value) {
-		if(!empty($value)) {
-			$this->attributes['periodo_inicial'] = Carbon::createFromFormat('d/m/Y', $value)->startOfDay();
-		}
-		else {
-			$this->attributes['periodo_inicial'] = null;
-		}
-	}
+    /**
+     * Setters Personalizados
+     */
 
-	public function setPeriodoFinalAttribute($value) {
-		if(!empty($value)) {
-			$this->attributes['periodo_final'] = Carbon::createFromFormat('d/m/Y', $value)->startOfDay();
-		}
-		else {
-			$this->attributes['periodo_final'] = null;
-		}
-	}
-	
-	/**
-	 * Scopes
-	 */
+    public function setPeriodoInicialAttribute($value) {
+        if(!empty($value)) {
+            $this->attributes['periodo_inicial'] = Carbon::createFromFormat('d/m/Y', $value)->startOfDay();
+        }
+        else {
+            $this->attributes['periodo_inicial'] = null;
+        }
+    }
 
-	/**
-	 * Funciones
-	 */
-	 
-	/**
-	 * Relaciones Uno a Uno
-	 */
-	
-	/**
-	 * Relaciones Uno a muchos
-	 */
+    public function setPeriodoFinalAttribute($value) {
+        if(!empty($value)) {
+            $this->attributes['periodo_final'] = Carbon::createFromFormat('d/m/Y', $value)->startOfDay();
+        }
+        else {
+            $this->attributes['periodo_final'] = null;
+        }
+    }
 
-	/**
-	 * Relaciones Muchos a uno
-	 */
+    /**
+     * Scopes
+     */
 
-	public function socio() {
-		return $this->belongsTo(Socio::class, 'socio_id', 'id');
-	}
+    /**
+     * Funciones
+     */
 
-	public function modalidadAhorro() {
-		return $this->belongsTo(ModalidadAhorro::class, 'modalidad_ahorro_id', 'id');
-	}
-	
-	/**
-	 * Relaciones Muchos a Muchos
-	 */
+    /**
+     * Relaciones Uno a Uno
+     */
+
+    /**
+     * Relaciones Uno a muchos
+     */
+
+    /**
+     * Relaciones Muchos a uno
+     */
+
+    public function socio() {
+        return $this->belongsTo(Socio::class, 'socio_id', 'id');
+    }
+
+    public function modalidadAhorro() {
+        return $this->belongsTo(ModalidadAhorro::class, 'modalidad_ahorro_id', 'id');
+    }
+
+    /**
+     * Relaciones Muchos a Muchos
+     */
 }
