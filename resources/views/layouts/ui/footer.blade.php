390790341edd58c2d4e3efdb81a0f3e8e1ade9ba
Jhonny Montoya <jhonnymontoya@startlinesoft.com>
2021-09-30 21:46:39 -0500

    Se cambia de versión a 1.2.0
    Signed-off-by: Jhonny Montoya <jhonnymontoya@startlinesoft.com>

--- resources/views/layouts/ui/footer.blade.php
+++ resources/views/layouts/ui/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	<div class="float-right d-none d-sm-block">
-		<b>Versión</b> 1.1.1
+		<b>Versión</b> 1.2.0
 	</div>
 	<strong>Copyright &copy; 2019-{{ date("Y") }} <a href="https://i-core.co">I-Core <i class="far fa-registered"></i></a>.</strong> Todos los derechos reservados.
 </footer>
--- resources/views/layouts/uiConsulta/footer.blade.php
+++ resources/views/layouts/uiConsulta/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col"><strong>Copyright &copy; 2019-{{ date("Y") }} <a href="https://i-core.co">I-Core <i class="far fa-registered"></i></a>.</strong> Todos los derechos reservados.</div>
     	<div class="pull-right hidden-xs">
-    		<b>Versión</b> 1.1.1
+    		<b>Versión</b> 1.2.0
     	</div>
     </div>
 </footer>
